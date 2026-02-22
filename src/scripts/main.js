6ec816552c862bc90340c8306020b2d9d7290866
uselessmorning <shtoykoigor@ukr.net>
2026-02-22 20:49:49 +0100

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
 // write your code here
+const population = document.querySelectorAll('.population');
+const averageField = document.querySelector('.average-population');
+const totalField = document.querySelector('.total-population');
+
+let total = 0;
+const counts = [];
+
+population.forEach((el) => {
+  const number = Number(el.textContent.replaceAll(',', ''));
+
+  total += number;
+  counts.push(number);
+});
+
+const average = total / counts.length;
+
+totalField.textContent = total.toLocaleString('en-US');
+averageField.textContent = average.toLocaleString('en-US');
